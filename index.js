2810acc12ea282fd2b3021b64d9c7321236d2d7e
irrssue <irrssue@gmail.com>
2023-11-15 15:05:42 +0630

    end of for loop

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-for (let i = 100; i >= 0; i-=10) {
-  console.log(i);
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let user in people) {
+  console.log(people[user].toUpperCase());
+  for (let i = 0; i < 10; i++) {
+    console.log(`This is thrid loop ${i}`)
+  }
 }
