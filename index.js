499691f4e601323d6e94caeaa72a48ebfaa45026
irrssue <irrssue@gmail.com>
2023-11-19 15:28:40 +0630

    random number guess

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-let guessnumber = ""
+let user = Math.floor(Math.random() * 10);
+let targetguess = Math.floor(Math.random() * 10);
+
+while (true) {
+  guess = Math.floor(Math.random() * 10);
+  if (guess === targetguess) {
+    console.log(`Correct The guess number was ${guess}`)
+    break;
+  }
+}
