a9e87d23f9caaefb66e2c63f8406dfe05ef38c98
irrssue <irrssue@gmail.com>
2023-11-23 04:04:35 +0630

    looping over Objects

--- index.js
+++ index.js
@@ -60,10 +60,20 @@
 //     }
 //   }
 
-const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+// const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
 
-// WRITE YOUR LOOP BELOW THIS LINE:
-for (let value of numbers) {
-    value = value * value;
-    console.log(value);
+// // WRITE YOUR LOOP BELOW THIS LINE:
+// for (let value of numbers) {
+//     value = value * value;
+//     console.log(value);
+// }
+
+const socre = {
+  name: "kenny",
+  age: 15,
+  howhedoing: "good",
+}
+
+for (let use of Object.keys(socre)) {
+  console.log(use);
 }
